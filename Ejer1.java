9b2b778c60e10de1a9717903b20d8eac6ea97eea
YisuSake05 <jsolisverastegui2007@gmail.com>
2026-09-13 21:50:17 -0500

    Primer commit del ejercicio

---
+++ Ejer1.java
@@ -0,0 +1,37 @@
+public class Ejer1 {
+
+
+public class Calculadora{
+
+public int suma(int a, int b){
+
+return a + b; 
+
+}
+
+public int suma(int a, int b, int c){
+
+    return a + b + c;
+}
+
+public double suma(double a, double b){
+
+    return a + b; 
+}
+
+}
+
+
+
+
+
+public static void main (String args []) {
+
+
+
+
+
+}
+
+    
+}
